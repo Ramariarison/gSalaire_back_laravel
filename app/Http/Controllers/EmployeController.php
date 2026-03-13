26127b49fd0cefe7defe1b0370b97e387dabe2af
Ramariarison <andryramariarison@gmail.com>
2026-03-13 11:00:08 +0300

    feat: Implémentation du controleur EmployeController pour la gestion des employés

---
+++ app/Http/Controllers/EmployeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employe;
+use Illuminate\Http\Request;
+
+class EmployeController extends Controller
+{
+    // Méthode pour récuperer tous les employés
+    public function index()
+    {
+        return response()->json(Employe::all());
+    }
+
+    // Méthode pour ajouter Employé
+    public function store(Request $request)
+    {
+        $employe = Employe::create([
+            'nom' => $request->nom,
+            'salaire' => $request->salaire
+        ]);
+
+        return response()->json([
+            "message" => "Employé ajouté avec succés",
+            "data" => $employe
+        ]);
+    }
+
+    // Méthode pour modifier un employé
+    public function update(Request $request, $id)
+    {
+        $employe = Employe::findOrFail($id);
+
+        $employe->update([
+            'nom' => $request->nom,
+            'salaire' => $request->salaire
+        ]);
+
+        return response()->json([
+            "message" => "Employé modifié avec succés",
+            "data" => $employe
+        ]);
+    }
+
+    // Méthode pour supprimer un employé
+    public function delete($id)
+    {
+        $employe = Employe::findOrFail($id);
+
+        $employe->delete();
+
+        return response()->json([
+            "message" => "Employé supprimé avec succés"
+        ]);
+    }
+
+    // Méthode pour retourner les stats à afficher dans le coté client
+    public function stats()
+    {
+        $total = Employe::sum('salaire');
+        $min = Employe::min('salaire');
+        $max = Employe::max('salaire');
+
+        return response()->json([
+            "salaire_total" => $total,
+            "salaire_min" => $min,
+            "salaire_max" => $max
+        ]);
+    }
+}
